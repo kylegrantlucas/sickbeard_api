a28c51861a44b2df9852ccd0457295b062d1d9cf
Kyle Lucas <kglucas93@gmail.com>
2016-03-15 22:34:35 -0700

    Significantly clean up gem and use metaprogramming to create endpoints for flexibility and consistency.

--- lib/sickbeard_api.rb
+++ lib/sickbeard_api.rb
@@ -1,8 +1,4 @@
 require "sickbeard_api/version"
-require "sickbeard_api/episodes"
-require "sickbeard_api/shows"
-require "sickbeard_api/logs"
-require "sickbeard_api/history"
 require "sickbeard_api/client"
 
 module SickbeardApi
--- lib/sickbeard_api/client.rb
+++ lib/sickbeard_api/client.rb
@@ -1,7 +1,4 @@
 require 'takeout'
-require 'sickbeard_api/shows'
-require 'sickbeard_api/episodes'
-require 'sickbeard_api/history'
 
 module SickbeardApi
   class Client
@@ -12,12 +9,14 @@ module SickbeardApi
       path = "/#{"#{options[:proxy_path]}/" if options[:proxy_path]}api/#{options[:api_key]}/"
       schemas = {get: {root: path}}
       self.client = Takeout::Client.new(uri: options[:uri], port: options[:port], schemas: schemas)
+      self.setup_endpoints
+      self.client
     end
 
     def setup_endpoints
       sickbeard = self.client.get_root(cmd:'sb').body
       
-      sickbeard.data.api_commands.each do |api_command|
+      sickbeard["data"]["api_commands"].each do |api_command|
         parsed_method_name = api_command.split('.').join('_')
         self.define_singleton_method(parsed_method_name) do |options={}|
           self.client.get_root({cmd: api_command}.merge(options))
